da6a9ea4bd65401d2d7ffd6e5121755e763faf30
Solomon Oduyebo <124704556+SolomonGM@users.noreply.github.com>
2024-03-26 07:51:11 +0000

    Update Die.cs

--- Die.cs
+++ Die.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Three_Class_Task
+{
+    public interface IRollable
+    {
+        int Roll();
+    }
+    class Die : IRollable //Implements the IRollable interface
+    {
+        private int currentValue = 0; //Property to hold the current value of the dice
+
+        public int Roll() //Method to roll dice with no parameters and returns the current value of the dice
+        {
+            Random random = new Random(); //Random object to generate random numbers
+            currentValue = random.Next(1, 7); //Edit values for Testing in testing module. Choooses random number between 1 and 6
+            return currentValue; //Returns the current value of the dice
+        }
+    }
+}
 
